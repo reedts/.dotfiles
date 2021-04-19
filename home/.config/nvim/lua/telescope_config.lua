abf91f17e1ff8b7c82a3714ab65134094273136f
reedts <j.reedts@gmail.com>
2021-04-19 09:24:54 +0200

    Switch to nerd-fonts

--- home/.config/nvim/lua/telescope_config.lua
+++ home/.config/nvim/lua/telescope_config.lua
@@ -13,7 +13,9 @@ require'telescope'.setup {
 				["<C-s>"] = actions.select_horizontal,
 				["<C-v>"] = actions.select_vertical
 			}
-		}
+		},
+		prompt_prefix = " ",
+		selection_caret = " "
 	},
 	extensions = {
 		fzy_native = {
@@ -24,3 +26,23 @@ require'telescope'.setup {
 }
 
 require'telescope'.load_extension('fzy_native')
+
+local M = {}
+M.search_dotfiles = function()
+	require("telescope.builtin").find_files({
+		prompt_title = "homesick",
+		cwd = "$HOME/.homesick/repos/.dotfiles/home",
+		hidden = true
+	})
+end
+
+
+M.search_config= function()
+	require("telescope.builtin").find_files({
+		prompt_title = "config",
+		cwd = "$HOME/.config",
+		hidden = true
+	})
+end
+
+return M
