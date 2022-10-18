a96b3c6ef25e2114987f7ae1077e46ee026e7e0e
reedts <j.reedts@gmail.com>
2022-10-18 10:23:18 +0200

    sparse: fix reaction function

--- home/.config/sparse/config.lua
+++ home/.config/sparse/config.lua
@@ -4,13 +4,23 @@ host("https://matrix.intxeighty.de")
 bind('e', 'visual', run_all(start_edit, deselect_message, switch_mode("insert-line")))
 
 bind('a', 'visual', function(c)
-	os.execute("rofi -mode emoji -show emoji")
-	content = c:get_clipboard()
+	local pipe = io.popen("rofi -mode emoji -show emoji -emoji-mode stdout", "r")
+
+	if pipe == nil then
+		error("Failed to run emoji selector")
+	end
+
+	local content = string.gsub(pipe:read("*a"), "\n", "")
+	pipe:close()
+	if content == nil or content == "" then
+		error("No content")
+	end
+
 	return c:react(content)
 end)
 
 bind('ss', 'normal', function(c)
-	file = os.tmpname() .. ".png"
+	local file = os.tmpname() .. ".png"
 	os.execute("flameshot gui -p " .. file)
 	return c:send_file(file)
 end)
