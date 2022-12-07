a74ef9ad0cc017fcb412b2ce2273f6a89c634070
reedts <j.reedts@gmail.com>
2022-12-07 11:56:58 +0100

    nvim: add isort to python lsp

--- home/.config/nvim/lua/lsp_config.lua
+++ home/.config/nvim/lua/lsp_config.lua
@@ -61,7 +61,8 @@ local null_ls = require('null-ls')
 null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.yapf,
-		null_ls.builtins.diagnostics.flake8
+		null_ls.builtins.diagnostics.flake8,
+		null_ls.builtins.formatting.isort
 	}
 })
 
