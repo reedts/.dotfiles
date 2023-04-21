3e48aaa90945bf8afa4e925c0f111c448072c1be
reedts <j.reedts@gmail.com>
2023-04-21 09:40:40 +0200

    treesitter: disable highlighting for languages with lsp support for that

--- home/.config/nvim/lua/treesitter_config.lua
+++ home/.config/nvim/lua/treesitter_config.lua
@@ -1,62 +1,62 @@
 require'nvim-treesitter.configs'.setup {
-	ensure_installed = { "c", "cpp", "lua", "python", "rust" },
+	ensure_installed = { "lua", "python" },
 	highlight = {
 		enable = true
 	},
-	-- textobjects = {
-	-- 	select = {
-	-- 		enable = true,
-	-- 		-- Automatically jump forward to textobj, similar to targets.vim
-	-- 		lookahead = true,
+	textobjects = {
+		select = {
+			enable = true,
+			-- Automatically jump forward to textobj, similar to targets.vim
+			lookahead = true,
 
-	-- 		keymaps = {
-	-- 			-- You can use the capture groups defined in textobjects.scm
-	-- 			["af"] = "@function.outer",
-	-- 			["if"] = "@function.inner",
-	-- 			["ac"] = "@class.outer",
-	-- 			["ic"] = "@class.inner",
-	-- 			["il"] = "@loop.inner",
-	-- 			["al"] = "@loop.outer",
-	-- 			["ab"] = "@block.outer",
-	-- 			["as"] = "@statement.outer"
-	-- 		}
-	-- 	},
-	-- 	swap = {
-	-- 		enable = true,
-	-- 		swap_next = {
-	-- 			["<leader>a"] = "@parameter.inner",
-	-- 		},
-	-- 		swap_previous = {
-	-- 			["<leader>A"] = "@parameter.inner",
-	-- 		},
-	-- 	},
-	-- 	move = {
-	-- 		enable = true,
-	-- 		set_jumps = true, -- whether to set jumps in the jumplist
-	-- 		goto_next_start = {
-	-- 			["]m"] = "@function.outer",
-	-- 			["]]"] = "@class.outer",
-	-- 		},
-	-- 		goto_next_end = {
-	-- 			["]M"] = "@function.outer",
-	-- 			["]["] = "@class.outer",
-	-- 		},
-	-- 		goto_previous_start = {
-	-- 			["[m"] = "@function.outer",
-	-- 			["[["] = "@class.outer",
-	-- 		},
-	-- 		goto_previous_end = {
-	-- 			["[M"] = "@function.outer",
-	-- 			["[]"] = "@class.outer",
-	-- 		},
-	-- 	},
-	-- 	lsp_interop = {
-	-- 		enable = true,
-	-- 		border = 'none',
-	-- 		peek_definition_code = {
-	-- 			["<leader>ldf"] = "@function.outer",
-	-- 			["<leader>ldc"] = "@class.outer",
-	-- 		},
-	-- 	}
-	-- }
+			keymaps = {
+				-- You can use the capture groups defined in textobjects.scm
+				["af"] = "@function.outer",
+				["if"] = "@function.inner",
+				["ac"] = "@class.outer",
+				["ic"] = "@class.inner",
+				["il"] = "@loop.inner",
+				["al"] = "@loop.outer",
+				["ab"] = "@block.outer",
+				["as"] = "@statement.outer"
+			}
+		},
+		swap = {
+			enable = true,
+			swap_next = {
+				["<leader>a"] = "@parameter.inner",
+			},
+			swap_previous = {
+				["<leader>A"] = "@parameter.inner",
+			},
+		},
+		move = {
+			enable = true,
+			set_jumps = true, -- whether to set jumps in the jumplist
+			goto_next_start = {
+				["]m"] = "@function.outer",
+				["]]"] = "@class.outer",
+			},
+			goto_next_end = {
+				["]M"] = "@function.outer",
+				["]["] = "@class.outer",
+			},
+			goto_previous_start = {
+				["[m"] = "@function.outer",
+				["[["] = "@class.outer",
+			},
+			goto_previous_end = {
+				["[M"] = "@function.outer",
+				["[]"] = "@class.outer",
+			},
+		},
+		lsp_interop = {
+			enable = true,
+			border = 'none',
+			peek_definition_code = {
+				["<leader>ldf"] = "@function.outer",
+				["<leader>ldc"] = "@class.outer",
+			},
+		}
+	}
 }
