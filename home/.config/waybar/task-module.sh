50395a11a62ac69ff7d43eca464dce5d92bbbbc5
reedts <j.reedts@gmail.com>
2023-04-24 10:43:12 +0200

    waybar: add task module

--- home/.config/waybar/task-module.sh
+++ home/.config/waybar/task-module.sh
@@ -5,8 +5,6 @@ COLOR_TODAY='#f99157'
 COLOR_WEEK='#ffcc66'
 COLOR_OK='#99cc99'
 
-NUMBER_FONT='T4'
-
 CHAR_OVERDUE=''
 CHAR_TODAY=''
 CHAR_WEEK='ﭷ'
@@ -16,19 +14,17 @@ NUM_TODAY=$(task +PENDING +DUETODAY -OVERDUE count 2> /dev/null)
 NUM_WEEK=$(task +PENDING +WEEK -DUETODAY count 2> /dev/null)
 
 if [[ $NUM_OVERDUE -eq 0 && $NUM_TODAY -eq 0 && $NUM_WEEK -eq 0 ]]; then
-	echo -n "%{F$COLOR_OK}%{F-}"
+	echo "{\"text\": \" \", \"class\": \"ok\"}"
 fi
 
 if [ $NUM_OVERDUE -gt 0 ]; then
-	echo -n "%{F$COLOR_OVERDUE}[ $CHAR_OVERDUE %{$NUMBER_FONT}$NUM_OVERDUE%{T-} ]%{F-} "
+	echo "{\"text\": \"[ $CHAR_OVERDUE $NUM_OVERDUE ]\", \"class\": \"overdue\"}"
 fi
 
 if [ $NUM_TODAY -gt 0 ]; then
-	echo -n "%{F$COLOR_TODAY}[ $CHAR_TODAY %{$NUMBER_FONT}$NUM_TODAY%{T-} ]%{F-} "
+	echo "{\"text\":\"[ $CHAR_TODAY $NUM_TODAY ]\",\"class\": \"today\"}"
 fi
 
 if [ $NUM_WEEK -gt 0 ]; then
-	echo -n "%{F$COLOR_WEEK}[ $CHAR_WEEK %{$NUMBER_FONT}$NUM_WEEK%{T-} ]%{F-}"
+	echo "{\"text\": \"[ $CHAR_WEEK $NUM_WEEK ]\", \"class\": \"week\"}"
 fi
-
-echo ""
